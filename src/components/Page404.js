23fbe9a54825e46a8ab1fa20bb2ee8f17f6abc67
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-18 17:14:42 -0500

    Create 404 page component

---
+++ src/components/Page404.js
@@ -0,0 +1,12 @@
+import React from "react";
+// Page404 component for displaying a user-friendly message when route doesn't exist
+const Page404 = () => {
+  return (
+    <div>
+      <h3>404, Page Not Found!</h3>
+      <p>The page you are looking for doesn't seem to exist</p>
+    </div>
+  );
+};
+
+export default Page404;
